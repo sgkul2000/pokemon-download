8ee0dd480ea2ea8dcdcafea884696431eb5bfaca
Shreesh Kulkarni <sgkul2000@gmail.com>
2021-05-07 23:45:27 +0530

    Added download progress

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sgkul2000/pokemon-download
 
 go 1.16
+
+require (
+	github.com/apoorvam/goterminal v0.0.0-20180523175556-614d345c47e5
+	github.com/mattn/go-isatty v0.0.12 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/apoorvam/goterminal"
 )
 
 func main() {
@@ -25,44 +31,19 @@ func main() {
 	}
 
 	if _, err := os.Stat("Pokemon"); os.IsNotExist(err) {
-		fmt.Println("hello")
 		os.Mkdir("Pokemon", 0777)
 	}
+
+	start := time.Now()
+
 	for ; i <= n; i++ {
 		url := fmt.Sprintf("https://streamcdnuservi4api.in/xiWvvKV/pokmn/pkmn1_IL/Dub/%v_Pokemon_Dubbed_Pokemon360.com.mp4", GetNumber(i))
-		err := DownloadFile(url, fmt.Sprintf("Pokemon/Pokemon-%v", GetNumber(i)))
-		if err != nil {
-			panic(err)
-		}
+		DownloadFile(url, fmt.Sprintf("Pokemon-%v.mp4", GetNumber(i)))
 	}
 
-}
+	elapsed := time.Since(start)
+	fmt.Printf("Download completed in %s\n", elapsed)
 
-// DownloadFile will download a url and store it in local filepath.
-// It writes to the destination file as it downloads it, without
-// loading the entire file into memory.
-func DownloadFile(url string, filepath string) error {
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func GetNumber(i int) string {
@@ -71,4 +52,107 @@ func GetNumber(i int) string {
 	} else {
 		return fmt.Sprint(i)
 	}
+}
+
+func PrintDownloadPercent(done chan int64, path string, total int64, file string) {
+
+	var stop bool = false
+	writer := goterminal.New(os.Stdout)
+
+	for {
+		select {
+		case <-done:
+			writer.Clear()
+			writer.Reset()
+			fmt.Printf("Downloaded: %v\n", file)
+			stop = true
+		default:
+
+			file, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fi, err := file.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			size := fi.Size()
+
+			if size == 0 {
+				size = 1
+			}
+
+			var percent float64 = float64(size) / float64(total) * 100
+			// fmt.Println(percent)
+			writer.Clear()
+			// add your text to writer's buffer
+			fmt.Fprintf(writer, "Downloading %v [%v>%v]\n", strings.Repeat("=", int(percent/4)), file, strings.Repeat(" ", int((100-percent)/4)))
+
+			// write to terminal
+			writer.Print()
+
+		}
+
+		if stop {
+			break
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func DownloadFile(url string, dest string) {
+
+	file := dest
+
+	var path bytes.Buffer
+	path.WriteString("Pokemon")
+	path.WriteString("/")
+	path.WriteString(file)
+
+	out, err := os.Create(path.String())
+
+	if err != nil {
+		fmt.Println(path.String())
+		panic(err)
+	}
+
+	defer out.Close()
+
+	headResp, err := http.Head(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer headResp.Body.Close()
+
+	size, err := strconv.Atoi(headResp.Header.Get("Content-Length"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	done := make(chan int64)
+
+	go PrintDownloadPercent(done, path.String(), int64(size), file)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	n, err := io.Copy(out, resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	done <- n
+
 }
